73d95e560c67bcfa827ad5593c4113fac80a44dc
Luis Felipe Graciolli <shiwa101@proton.me>
2025-08-13 10:49:52 -0300

    feat: Atualiza lógica de updateTaskById para modificar somente campos alterados e registra data de atualização

--- src/main/java/com/graciolli/stm/Service/TaskService.java
+++ src/main/java/com/graciolli/stm/Service/TaskService.java
@@ -4,11 +4,12 @@ import com.graciolli.stm.Repository.Task;
 import com.graciolli.stm.Repository.TaskRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
 public class TaskService {
-    private TaskRepository taskRepository;
+    private final TaskRepository taskRepository;
 
     public TaskService(TaskRepository taskRepository){
         this.taskRepository = taskRepository;
@@ -32,9 +33,13 @@ public class TaskService {
     TODO: Figure out how to manage tasks requests without all fields to update
     TODO: Build or find alternatives to the Builder Pattern!
     */
-    public void updateTaskById(Integer id, Task taskUpdated){
+    public void updateTaskById(Integer id, Task taskUpdated) {
         Task taskEntity = readTasksById(id);
-        taskRepository.saveAndFlush(taskUpdated);
+        taskEntity.setTitle(taskUpdated.getTitle());
+        taskEntity.setDescription(taskUpdated.getDescription());
+        taskEntity.setStatus(taskUpdated.getStatus());
+        taskEntity.setUpdatedAt(new Date());
+        taskRepository.saveAndFlush(taskEntity);
     }
 
     public void deleteTaskById(Integer id){
